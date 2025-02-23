f33b2ed98c084e552b1df78a2270b32538cab91d
DESKTOP-UK8MVSK\User <wanutpongbb@gmail.com>
2025-02-23 13:43:19 +0700

    add docker

---
+++ docker/entrypoint.sh
@@ -0,0 +1,12 @@
+# #!/bin/bash
+
+# # Run migrations
+# php artisan migrate --force
+
+# # Run database seeding
+# php artisan db:seed --force
+
+# php artisan storage:link
+
+# # Start PHP-FPM
+# exec "$@"
